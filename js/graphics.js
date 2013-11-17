40166dc1de8baa8ead28715336a992370f6c817f
Julien Fleury <julien.fleury@ensimag.fr>
2013-11-17 15:48:37 +0100

    merge

--- js/graphics.js
+++ js/graphics.js
@@ -12,51 +12,51 @@ var Screen = function(callback) {
     var nbSpritesLoading = 0, nbSpritesLoaded = 0;
 
     var spritesImg = {
-	// Players
-	'player13': 'assets/amg1_bk1.png',
-	'player14': 'assets/amg1_fr1.png',
-	'player12': 'assets/amg1_lf1.png',
-	'player11': 'assets/amg1_rt1.png',
-	'player23': 'assets/npc5_bk1.png',
-	'player24': 'assets/npc5_fr1.png',
-	'player22': 'assets/npc5_lf1.png',
-	'player21': 'assets/npc5_rt1.png',
+	    // Players
+	    'player13': 'assets/amg1_bk1.png',
+	    'player14': 'assets/amg1_fr1.png',
+	    'player12': 'assets/amg1_lf1.png',
+	    'player11': 'assets/amg1_rt1.png',
+	    'player23': 'assets/npc5_bk1.png',
+	    'player24': 'assets/npc5_fr1.png',
+	    'player22': 'assets/npc5_lf1.png',
+	    'player21': 'assets/npc5_rt1.png',
 
-	// Monsters
-	'monster13': 'assets/scr1_bk1.png',
-	'monster14': 'assets/scr1_fr1.png',
-	'monster12': 'assets/scr1_lf1.png',
-	'monster11': 'assets/scr1_rt1.png',
-	'monster23': 'assets/wmg1_bk1.png',
-	'monster24': 'assets/wmg1_fr1.png',
-	'monster22': 'assets/wmg1_lf1.png',
-	'monster21': 'assets/wmg1_rt1.png',
+	    // Monsters
+	    'monster13': 'assets/scr1_bk1.png',
+	    'monster14': 'assets/scr1_fr1.png',
+	    'monster12': 'assets/scr1_lf1.png',
+	    'monster11': 'assets/scr1_rt1.png',
+	    'monster23': 'assets/wmg1_bk1.png',
+	    'monster24': 'assets/wmg1_fr1.png',
+	    'monster22': 'assets/wmg1_lf1.png',
+	    'monster21': 'assets/wmg1_rt1.png',
 
-	// Elements
-	'fire': 'assets/conjure_ball_lightning.png',
+	    // Elements
+	    'fire': 'assets/conjure_ball_lightning.png',
 
-	// Labyrinthurtoi
-	'wall': 'assets/stone_brick12.png',
-	'floor': 'assets/crystal_floor3.png',
-	'arena': 'assets/rough_red0.png'
+	    // Labyrinthurtoi
+	    'wall': 'assets/stone_brick12.png',
+	    'floor': 'assets/crystal_floor3.png',
+	    'arena': 'assets/rough_red0.png',
         'table': 'assets/cestpourtoibenjamin.png'
 
     }
 
     this.sprites = [];
-    for (var spriteName in spritesImg) {
-	nbSpritesLoading++;
-	var sprite = new Image();
-	sprite.onload = function () {
-	    nbSpritesLoaded++;
+        for (var spriteName in spritesImg) {
+	    nbSpritesLoading++;
+	    var sprite = new Image();
+	    sprite.onload = function () {
+	        nbSpritesLoaded++;
 
-	    if (nbSpritesLoaded === nbSpritesLoading) {
-		nbSpritesLoaded = -Infinity;
-		callback();
+	        if (nbSpritesLoaded === nbSpritesLoading) {
+		        nbSpritesLoaded = -Infinity;
+		        callback();
+	        }
 	    }
-	}
-	sprite.src = spritesImg[spriteName];
-	this.sprites[spriteName] = sprite;
+	    sprite.src = spritesImg[spriteName];
+	    this.sprites[spriteName] = sprite;
     }
 }
 var screen;
@@ -97,7 +97,7 @@ Screen.prototype = {
 }
     
 var MapGraphic = function (labyrinth) {
-	this.labyrinth = labyrinth
+	this.labyrinth = labyrinth;
 }
 
 MapGraphic.prototype = {
@@ -105,21 +105,18 @@ MapGraphic.prototype = {
 	    // Parcours de la matrice et affichage d'un 
 	    // carré de couleur différente pour chaque nombre
 	    for (y = 0; y < this.labyrinth.getHeight(); y++ ) {
-		for (x = 0; x < this.labyrinth.getWidth(); x++ ) {
-		    var type = parseInt(this.labyrinth.get(x, y));
-		    if (!this.is_visible(origin_x, origin_y, visionScope, x, y) || type == CaseCode.UNDEFINED) {
-			screen.printRect(32*x,32*y,32,32, "rgba(255,0,0,1)");
-		    } else if (type == CaseCode.WALL) {
-			screen.drawWall(32*x,32*y);
-		    } else if (type == CaseCode.GROUND) {
-			screen.drawFloor(32*x,32*y);
-
-		    }
-		    
-		    else if (type == CaseCode.ARENA) {
-			screen.drawArena(32*x,32*y);
-		    }	
-		}		
+		    for (x = 0; x < this.labyrinth.getWidth(); x++ ) {
+		        var type = parseInt(this.labyrinth.get(x, y));
+		        if (!this.is_visible(origin_x, origin_y, visionScope, x, y) || type == CaseCode.UNDEFINED) {
+			        screen.printRect(32*x,32*y,32,32, "rgba(255,0,0,1)");
+		        } else if (type == CaseCode.WALL) {
+			        screen.drawWall(32*x,32*y);
+		        } else if (type == CaseCode.GROUND) {
+			        screen.drawFloor(32*x,32*y);
+		        } else if (type == CaseCode.ARENA) {
+			        screen.drawArena(32*x,32*y);
+		        }	
+		    }		
 	    }
 	},
 	
@@ -128,16 +125,16 @@ MapGraphic.prototype = {
     	    var diff_y = y - origin_y;
     	    return (diff_x < visionScope && diff_y < visionScope && diff_x > -visionScope && diff_y > -visionScope);
 	}
-    }
+}
 
-    var Graphics = function (callback) {
+var Graphics = function (callback) {
 	this.mapGraphic = null;
 
 	screen = new Screen(callback);
 	screen.printRect(0, 0, screen.width, screen.height, "rgba(255, 255, 255, 0.5)");
-    }
+}
 
-    Graphics.prototype = {
+Graphics.prototype = {
 	setLabyrinth: function (labyrinth) {
 	    if (this.mapGraphic) {
 		this.mapGraphic.setMatrix(labyrinth);
@@ -152,22 +149,21 @@ MapGraphic.prototype = {
 	    screen.playersMapCxt.clearRect(0, 0, 736, 1024);
 
 	    for (var i in entities) {
-		var entity = entities[i];
-		var pos = entity.getPosition();
-		var spriteId = entity.getSpriteId();
+		    var entity = entities[i];
+		    var pos = entity.getPosition();
+		    var spriteId = entity.getSpriteId();
 
-		if (spriteId == SpriteCode.PLAYER1) {
-		    screen.drawPlayer(1, 32 * pos.x, 32 * pos.y, entity.getDirection());
-		} else if (spriteId == SpriteCode.PLAYER2) {
-		    screen.drawPlayer(2, 32 * pos.x, 32 * pos.y, entity.getDirection());
-		} else if (spriteId == SpriteCode.MONSTER1) {
-		    screen.drawMonster(1, 32*pos.x,32*pos.y, entity.getDirection());
-		} else if (spriteId == SpriteCode.MONSTER2) {
-		    screen.drawMonster(2, 32*pos.x,32*pos.y, entity.getDirection());
-		} else if (spriteId == SpriteCode.FIRE_BALL) {
-		    screen.drawFire(32*pos.x, 32*pos.y);
-		}
-
+		    if (spriteId == SpriteCode.PLAYER1) {
+		        screen.drawPlayer(1, 32 * pos.x, 32 * pos.y, entity.getDirection());
+		    } else if (spriteId == SpriteCode.PLAYER2) {
+		        screen.drawPlayer(2, 32 * pos.x, 32 * pos.y, entity.getDirection());
+		    } else if (spriteId == SpriteCode.MONSTER1) {
+		        screen.drawMonster(1, 32*pos.x,32*pos.y, entity.getDirection());
+		    } else if (spriteId == SpriteCode.MONSTER2) {
+		        screen.drawMonster(2, 32*pos.x,32*pos.y, entity.getDirection());
+		    } else if (spriteId == SpriteCode.FIRE_BALL) {
+		        screen.drawFire(32*pos.x, 32*pos.y);
+		    }
 	    }
 	}
 }
